34df8f02cebe6f10e37ef797e473f031eb5d1f2c
Pratik Khamar <pratik@krishtechnolabs.com>
2016-08-11 10:16:59 +0530

    update code compatible with 2.1

---
+++ Company/Categorylist/Model/Category/DataProvider.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Company\Categorylist\Model\Category;
+
+/**
+ * Class DataProvider
+ *
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class DataProvider extends \Magento\Catalog\Model\Category\DataProvider
+{
+    /**
+     * @return array
+     */
+    protected function getFieldsMap()
+    {
+        return [
+            'general' =>
+                [
+                    'parent',
+                    'path',
+                    'is_active',
+                    'include_in_menu',
+                    'name',
+                ],
+            'content' =>
+                [
+                    'thumbnail',
+                    'image',
+                    'description',
+                    'landing_page',
+                ],
+            'display_settings' =>
+                [
+                    'display_mode',
+                    'is_anchor',
+                    'available_sort_by',
+                    'use_config.available_sort_by',
+                    'default_sort_by',
+                    'use_config.default_sort_by',
+                    'filter_price_range',
+                    'use_config.filter_price_range',
+                ],
+            'search_engine_optimization' =>
+                [
+                    'url_key',
+                    'url_key_create_redirect',
+                    'use_default.url_key',
+                    'url_key_group',
+                    'meta_title',
+                    'meta_keywords',
+                    'meta_description',
+                ],
+            'assign_products' =>
+                [
+                ],
+            'design' =>
+                [
+                    'custom_use_parent_settings',
+                    'custom_apply_to_products',
+                    'custom_design',
+                    'page_layout',
+                    'custom_layout_update',
+                ],
+            'schedule_design_update' =>
+                [
+                    'custom_design_from',
+                    'custom_design_to',
+                ],
+            'category_view_optimization' =>
+                [
+                ],
+            'category_permissions' =>
+                [
+                ],
+        ];
+    }
+}
